3907d4fc20d114d2369409ed60382489bf6c6fff
Michael <michael.spengler@sap.com>
2022-02-05 14:03:37 +0100

    add map

--- client-with-svelte/src/components/Search.svelte
+++ client-with-svelte/src/components/Search.svelte
@@ -1,9 +1,12 @@
 <script>
+    import Map from "https://deno.land/x/svelte_map/Maps.svelte";
+
     export let location;
 </script>
 
 <p>Where do you want to exchange crypto?</p>
 <br />
+<Map long="8.46694" lat="49.4891" button={true} />
 <p>
     <input bind:value={location} placeholder="...start typing" />
 </p>
