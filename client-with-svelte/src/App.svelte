a19c7de4739b1de9eeb1c754784ed4366538d13e
Michael <michael.spengler@sap.com>
2022-02-05 13:45:57 +0100

    first commit

---
+++ client-with-svelte/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Welcome from "@/components/Welcome.svelte";
+  import { fade } from "svelte/transition";
+
+  let title = "Peer 2 Peer Exchange";
+</script>
+
+<main transition:fade>
+  <Welcome {title} />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
